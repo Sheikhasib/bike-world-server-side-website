e37df39a24eb50a30f8a3bcada0ca80b12ce7f89
Sheikhasib <sheikhhasib037@gmail.com>
2021-11-12 00:35:36 +0600

    users added

--- index.js
+++ index.js
@@ -24,15 +24,78 @@ async function run() {
   try {
     await client.connect();
     // console.log("database connected successfully");
-    const database = client.db('bike_world');
-    const productsCollection = database.collection('products');
+    const database = client.db("bike_world");
+    const productsCollection = database.collection("products");
+    const reviewCollection = database.collection("review");
+    const ordersCollection = database.collection("orders");
+    const usersCollection = database.collection("users");
 
     // get services
-    app.get('/products', async(req, res) => {
-        const cursor = productsCollection.find({});
-        const products = await cursor.toArray();
-        res.send(products);
-    })
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    // get single product
+    app.get("/singleProduct/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await productsCollection
+        .find({ _id: ObjectId(req.params.id) })
+        .toArray();
+      res.send(result[0]);
+      console.log(result);
+    });
+
+    // review
+    app.post("/addSReview", async (req, res) => {
+      const result = await reviewCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // insert order and
+    app.post("/addOrders", async (req, res) => {
+      const result = await ordersCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // From useFirebase to get special user information
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // From useFirebase to post user data
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+    // From useFirebase to Updating user data
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      // console.log('put', user);
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
   } 
   finally {
     // await client.close();
